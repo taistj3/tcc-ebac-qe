2014b54ed7f56c8eb82349384aa23ff525c2e17d
Hellen Taís <taistj3@gmail.com>
2025-10-02 16:41:29 -0400

    update cypress.config e package.json

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    specPattern: 'projetoTCC/automacoes/testeUI/cypress/e2e/**/*.cy.{js,ts}',
+    supportFile: 'projetoTCC/automacoes/testeUI/cypress/support/e2e.js',
+    fixturesFolder: 'projetoTCC/automacoes/testeUI/cypress/fixtures',
+    screenshotsFolder: 'projetoTCC/automacoes/testeUI/cypress/screenshots'
   },
 });
