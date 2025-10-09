41d2b2346cb09e2c3dcc6f00b68f0c809b8a81b6
Hellen Taís <taistj3@gmail.com>
2025-10-09 17:39:30 -0400

    correções

--- projetoTCC/automacoes/testesMobile/specs/catalogo.test.js
+++ projetoTCC/automacoes/testesMobile/specs/catalogo.test.js
@@ -1,19 +1,13 @@
 import { expect, $$ } from '@wdio/globals'
 import CatalogoPage from '../pageObjects/catalogo.page.js'
 
-describe('Funcionalidade catálogo de produtos', () => {
+describe('Funcionalidade catálogo de produtos do aplicativo ebacShop', () => {
 
     it('Deve buscar produto por nome', async () => {
         await CatalogoPage.abrirCatalogo()
         await CatalogoPage.buscarProduto('bag pandora')
         const produto = await CatalogoPage.produtoPorNome('bag pandora')
         expect(await (await produto).isDisplayed()).toBe(true)
-        // const browse = await $('android=new UiSelector().text("Browse")');
-        // await browse.click();
-        // const searchInput = await $('id=searchInput');
-        // await searchInput.setValue('bag pandora');
-        // const produto = await $('android=new UiSelector().text("bag pandora")');
-        // expect(await (await produto)).toBeDisplayed();
     })
 
     it('Deve exibir detalhes do produto ao clicar', async () => {
@@ -21,20 +15,5 @@ describe('Funcionalidade catálogo de produtos', () => {
         await CatalogoPage.produtoPorIndexClick(0)
         const produtoDetalhe = await $('android=new UiSelector().text("Additional Details")').scrollIntoView();
         expect(await (await produtoDetalhe)).toBeDisplayed();
-
-        // await produto.waitForDisplayed({timeout:10000});
-        // await produto.click();
-        // expect(await ProdutoPage.validarDetalheVisivel()).toBe(true)
-
-        // const browse = await $('android=new UiSelector().text("Browse")');
-        // await browse.click();
-        // const produto = await $('android=new UiSelector().className("android.widget.ImageView").instance(0)');
-        // await produto.waitForDisplayed({timeout:10000});
-        // await produto.click();
-        // const produtoDetalhe = await $('android=new UiSelector().text("Additional Details")');
-        // expect(await (await produtoDetalhe)).toBeDisplayed();
-
-         // const produto = await CatalogoPage.produtoPorIndex(0)
-        // await produto.click()
     })
 })
